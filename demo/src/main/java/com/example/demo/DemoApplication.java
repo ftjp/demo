0bd02ac164c3dd35ef4a8376069739d07ac98967
laijianping <535678291@qq.com>
2024-11-14 17:34:06 +0800

    配置优化/ 工具类

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * description: 启动类
@@ -10,7 +11,8 @@ import org.springframework.context.annotation.PropertySource;
  * @author: LJP
  * @date: 2024/11/14 9:32
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.*"})
+@EnableAsync
 @PropertySource("classpath:constants.properties")
 public class DemoApplication {
 
---
+++ demo/src/main/java/com/example/demo/util/ParamCheckUtil.java
@@ -0,0 +1,67 @@
+package com.example.demo.util;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.text.CharSequenceUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * description:redis操作工具类
+ *
+ * @author: LJP
+ * @date: 2024/8/16 16:12
+ */
+@Slf4j
+@Component
+public class ParamCheckUtil {
+
+    /**
+     * description: 校验参数
+     *
+     * @param params 参数名，值（默认不允许为空 字符串/集合/数组）
+     * @author: LJP
+     * @date: 2024/9/19 15:34
+     */
+    public static void checkParamNotNull(Object... params) {
+        checkParamNotNull(false, params);
+    }
+
+    /**
+     * description: 校验参数
+     *
+     * @param allowEmpty 是否允许为空 字符串/集合/数组
+     * @param params     参数名，值
+     * @author: LJP
+     * @date: 2024/9/19 15:34
+     */
+    public static void checkParamNotNull(Boolean allowEmpty, Object... params) {
+        for (int i = 0; i < params.length; i += 2) {
+            String name = params[i].toString();
+            Object obj = params[i + 1];
+            if (obj == null) {
+                throw new RuntimeException(String.format("[%s]是null", name));
+            }
+            // 是否允许为空 字符串/集合/数组
+            if (allowEmpty) {
+                continue;
+            }
+            if (obj instanceof String && CharSequenceUtil.isBlank(((String) obj))) {
+                throw new RuntimeException(String.format("[%s]是空字符串", name));
+            }
+            if (obj instanceof Collection && CollUtil.isEmpty((Collection) obj)) {
+                throw new RuntimeException(String.format("[%s]是空集合", name));
+            }
+            if (obj instanceof Map && CollUtil.isEmpty((Map) obj)) {
+                throw new RuntimeException(String.format("[%s]是空Map", name));
+            }
+            if (obj.getClass().isArray() && Array.getLength(obj) == 0) {
+                throw new RuntimeException(String.format("[%s]是空数组", name));
+            }
+        }
+    }
+
+}
