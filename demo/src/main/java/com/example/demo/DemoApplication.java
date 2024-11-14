76d614bd66806d1de8968435f3ac3aeb7ea85c9a
放开追寻 <73630138+ftjp@users.noreply.github.com>
2024-11-14 10:09:59 +0800

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description: 启动类
+ *
+ * @author: LJP
+ * @date: 2024/11/14 9:32
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
